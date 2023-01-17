30bec6f37362f135e985cb8dc4977dbe48c413c0
Mohammed-Reda Daoudi <37927504+mredaoudi@users.noreply.github.com>
2023-01-17 16:52:33 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name='tap-freshdesk',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_freshdesk'],
       install_requires=[
-          'singer-python==5.2.3',
+          'singer-python @ git+https://github.com/peliqan-io/singer-python@master',
           'requests==2.20.0',
           'backoff==1.3.2'
       ],
